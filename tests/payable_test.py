441abc81ed7a291826b4e88c0435a98e7a4f71c3
Victor Messina <victormessina95@hotmail.com>
2017-03-16 14:58:19 -0300

    add method get_payable_by_id_fail

--- tests/payable_test.py
+++ tests/payable_test.py
@@ -2,9 +2,9 @@
 
 import mock
 
-from .mocks import fake_get_payable_by_id
+from .mocks import fake_get_payable_by_id,fake_request_fail
 from .pagarme_test import PagarmeTestCase
-from pagarme.payable import Payable
+from pagarme.payable import Payable,PagarmeApiError
 
 class PayableTestCase(PagarmeTestCase):
 
@@ -13,3 +13,9 @@ class PayableTestCase(PagarmeTestCase):
         payable = Payable(api_key='api_key')
         payable.find_by_id(906983)
         self.assertEqual(906983, payable.id)
+
+    @mock.patch('requests.get', mock.Mock(side_effect=fake_request_fail))
+    def test_get_payable_by_id_fails(self):
+        payable = Payable(api_key='apikey')
+        with self.assertRaises(PagarmeApiError):
+            payable.find_by_id(906983)
