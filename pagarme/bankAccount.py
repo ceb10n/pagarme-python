4bda37312efb20b614e4ad8160b85683f3ea9b8e
Victor Messina <victormessina95@hotmail.com>
2017-03-16 13:54:44 -0300

    add import BASE_URL and change this param in class body

--- pagarme/bankAccount.py
+++ pagarme/bankAccount.py
@@ -4,10 +4,11 @@ import requests
 
 from .exceptions import PagarmeApiError, NotPaidException, NotBoundException
 from .resource import AbstractResource
+from .settings import BASE_URL
 
 
 class BankAccount(AbstractResource):
-    BASE_URL = 'https://api.pagar.me/1/bank_accounts'
+    BASE_URL = BASE_URL + 'bank_accounts'
 
     def __init__(
         self,
